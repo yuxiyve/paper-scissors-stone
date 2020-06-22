4d484545d1c38dee0ce8c926afe8a1a23b293c83
yuxiyve <67279179+yuxiyve@users.noreply.github.com>
2020-06-22 23:04:34 +0800

    Update paper scisser stone.py

--- paper scisser stone.py
+++ paper scisser stone.py
@@ -1,13 +1,13 @@
 import random
 
-player = int( input("请输入石头（1）/ 剪刀（2）/ 布（3）："))
+player = int( input("please input stone（1）/ scissors（2）/ paper（3）："))
 computer = random.randint(1, 3)
-print("玩家的拳头是：", player, "-", "电脑的拳头是：", computer)
+print("you are：", player, "-", "computer is：", computer)
 if (player == 1 and computer == 2) or \
         (player == 2 and computer == 3) or \
         (player == 3 and computer == 1):
-    print(" winner winner chicken dinner!")
+    print(" winner winner, chicken dinner!")
 elif player == computer:
     print("not bad,try again!")
 else:
-    print("you are stupid~")
+    print("the computer is smarter than you~")
